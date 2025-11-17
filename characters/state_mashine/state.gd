57f79e33ba979acf833631192c69a54e963e771c
KaiserTorian <christianmeikl@gmail.com>
2025-11-17 21:48:11 +0100

    Meh, state mashine idea

---
+++ characters/state_mashine/state.gd
@@ -0,0 +1,14 @@
+class_name State
+extends Node
+
+var parent: Node3D
+
+func init() -> void:
+	pass
+
+func process(_delta: float) -> void:
+	pass
+
+func physics_process(_delta: float) -> void:
+	pass
+	
